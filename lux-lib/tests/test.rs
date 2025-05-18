6735cd360d0ed5c8545b89d5044a73dd95eee34e
Marc Jakobi <marc.jakobi@tiko.energy>
2025-05-18 22:45:33 +0200

    tests: integration test case for busted with nlua

--- lux-lib/tests/test.rs
+++ lux-lib/tests/test.rs
@@ -83,3 +83,35 @@ async fn non_regression_lockfile_corruption() {
         String::from_utf8(tokio::fs::read(project.lockfile_path()).await.unwrap());
     assert_eq!(lockfile_before_test, lockfile_after_test);
 }
+
+#[tokio::test]
+async fn run_busted_nlua_test() {
+    run_busted_nlua_test_impl(false).await
+}
+
+#[tokio::test]
+async fn run_busted_nlua_test_no_lock() {
+    run_busted_nlua_test_impl(true).await
+}
+
+async fn run_busted_nlua_test_impl(no_lock: bool) {
+    let project_root =
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("resources/test/sample-project-busted-nlua");
+    let temp_dir = assert_fs::TempDir::new().unwrap();
+    temp_dir.copy_from(&project_root, &["**"]).unwrap();
+    let project_root = temp_dir.path();
+    let project: Project = Project::from(project_root).unwrap().unwrap();
+    let tree_root = project.root().to_path_buf().join(".lux");
+    let _ = std::fs::remove_dir_all(&tree_root);
+
+    let config = ConfigBuilder::new()
+        .unwrap()
+        .user_tree(Some(tree_root))
+        .build()
+        .unwrap();
+    Test::new(project, &config)
+        .no_lock(no_lock)
+        .run()
+        .await
+        .unwrap();
+}

9543f10d900375336df97169d9b991115755e468
Marc Jakobi <marc.jakobi@tiko.energy>
2025-06-02 23:00:42 +0200

    docs: busted-nlua test backend

--- lux-lib/tests/test.rs
+++ lux-lib/tests/test.rs
@@ -84,16 +84,22 @@ async fn non_regression_lockfile_corruption() {
     assert_eq!(lockfile_before_test, lockfile_after_test);
 }
 
+#[cfg(target_os = "linux")]
 #[tokio::test]
 async fn run_busted_nlua_test() {
     run_busted_nlua_test_impl(false).await
 }
 
+#[cfg(target_os = "linux")]
 #[tokio::test]
 async fn run_busted_nlua_test_no_lock() {
     run_busted_nlua_test_impl(true).await
 }
 
+// NOTE: The busted-nlua test backend is currently broken on macOS and Windows.
+// On macOS, it appears that Neovim segfaults when `require`ing `lfs` (luafilesystem).
+// Investigation is needed on Windows.
+#[cfg(target_os = "linux")]
 async fn run_busted_nlua_test_impl(no_lock: bool) {
     let project_root =
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("resources/test/sample-project-busted-nlua");
